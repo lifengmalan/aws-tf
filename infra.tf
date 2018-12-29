c61cd8812a14ac0c600248be7010eb97128246a1
Feng LI <lifengmalan@gmail.com>
2018-12-29 13:57:33 +0800

    update infra.tf

--- infra.tf
+++ infra.tf
@@ -14,7 +14,7 @@ module "vpc" {
   azs             = ["${var.az_1}", "${var.az_2}", "${var.az_3}"]
   private_subnets = ["${var.private_subnet_1}", "${var.private_subnet_2}", "${var.private_subnet_3}"]
   public_subnets  = ["${var.public_subnet_1}", "${var.public_subnet_2}", "${var.public_subnet_3}"]
-  database_subnets = ["${var.private_subnet_1}", "${var.private_subnet_2}", "${var.private_subnet_3}"]
+  database_subnets = ["${var.database_subnet_1}", "${var.database_subnet_2}", "${var.database_subnet_3}"]
   
 
   assign_generated_ipv6_cidr_block = true
