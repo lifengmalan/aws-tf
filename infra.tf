e0b93b9be01e08ff47a7e4375c0d89cdf3ddbeae
Feng LI <lifengmalan@gmail.com>
2018-12-29 16:05:05 +0800

    update infra.tf

--- infra.tf
+++ infra.tf
@@ -108,7 +108,7 @@ module "db" {
   port     = "${var.port_rds}"
 
   #vpc_security_group_ids = ["${var.securitygroups}"]
-  vpc_security_group_ids = "${module.sg_rds.this_security_group_id}"
+  vpc_security_group_ids = ["${module.sg_rds.this_security_group_id}"]
 
   maintenance_window = "Mon:00:00-Mon:03:00"
   backup_window      = "03:00-06:00"
