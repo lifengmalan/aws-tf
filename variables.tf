c4d9e8b94baaf51af000aafe103a42d79dce25df
Feng LI <lifengmalan@gmail.com>
2018-12-29 12:56:01 +0800

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" { default = "ap-southeast-1" }
+
+variable "cidr" {}
+
+variable "az_1" {}
+variable "az_2" {}
+variable "az_3" {}
+
+variable "private_subnet_1" {}
+variable "private_subnet_2" {}
+variable "private_subnet_3" {}
+
+variable "public_subnet_1" {}
+variable "public_subnet_2" {}
+variable "public_subnet_3" {}
+
+variable "name_vpc" {}
+variable "owner_vpc" {}
+variable "env_vpc" {}
