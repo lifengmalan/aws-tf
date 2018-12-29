cf7fc50781baf856cc6cc56c53decfd1a82075ca
Feng LI <lifengmalan@gmail.com>
2018-12-29 14:25:15 +0800

    update infra.tf

--- infra.tf
+++ infra.tf
@@ -4,6 +4,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+# VPC
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -31,5 +32,59 @@ module "vpc" {
   vpc_tags = {
     Name = "${var.name_vpc}"
   }
+
+# Security Groups
+module "sg_prod" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name = "${var.name_sg}"
+  description = "${var.description_sg}"
+ 
+  vpc_id = "${module.vpc.vpc_id}"
+
+  use_name_prefix = "false"
+
+  ingress_cidr_blocks = ["${var.full_cidr}"]
+  ingress_rules       = "${var.in_rules}"
+
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port        = 2368
+      to_port          = 2368
+      protocol         = "tcp"
+      description      = "for ghost"
+      cidr_blocks = "${var.full_cidr}"
+    },
+    {
+      from_port        = 8080
+      to_port          = 8080
+      protocol         = "tcp"
+      description      = "http8080 for dolibar and opencart"
+      cidr_blocks = "${var.full_cidr}"
+    },
+  ]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+
+}
+
+module "sg_rds" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name = "${var.name_sg_rds}"
+  description = "${var.description_rds}"
+ 
+  vpc_id = "${module.vpc.vpc_id}"
+
+  use_name_prefix = "false"
+
+  ingress_cidr_blocks = ["${var.cidr}"]
+  ingress_rules       = ["mysql-tcp"] 
+
+  egress_cidr_blocks = ["${var.cidr}"]
+  egress_rules       = ["all-all"]
+}
 
 }
