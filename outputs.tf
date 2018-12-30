6eff377bb8c858b23855db731cb470f006feccf1
Feng LI <lifengmalan@gmail.com>
2018-12-30 10:34:28 +0800

    update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -91,6 +91,10 @@ output "public_dns" {
   description = "List of public DNS names assigned to the instances"
   value       = "${module.ec2.public_dns}"
 }
+output "public_ip" {
+  description = "List of public IP assigned to the instances"
+  value       = "${module.ec2.public_ip}"
+}
 output "vpc_security_group_ids" {
   description = "List of VPC security group ids assigned to the instances"
   value       = "${module.ec2.vpc_security_group_ids}"
